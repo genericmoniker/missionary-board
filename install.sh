8b2669fa77f69d1255ce50b356d037a6171b37e7
Eric Smith <eric@esmithy.net>
2025-02-02 19:27:30 -0700

    More de-Docker-ize

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 set -x
 set -euo pipefail
 
+git config pull.ff only
+
 # Install the backend service
 mkdir -p ~/.config/systemd/user
 cp system/mboard.service ~/.config/systemd/user/
+systemctl --user daemon-reload
 systemctl --user enable mboard
 systemctl --user start mboard
