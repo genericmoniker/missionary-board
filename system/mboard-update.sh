aeb9dc59427a7f0d283fd8cec22dc8ac2f15e28d
Eric Smith <eric@esmithy.net>
2025-02-17 11:54:18 -0700

    Fix update script

--- system/mboard-update.sh
+++ system/mboard-update.sh
@@ -15,7 +15,6 @@ if [ "$current_hash" != "$latest_hash" ]; then
     echo "System update available. Updating to commit hash ${latest_hash}..."
     git pull || { echo "Failed to pull latest changes"; exit 1; }
     ./install.sh || { echo "Failed to run install script"; exit 1; }
-    sudo systemctl restart mboard || { echo "Failed to restart mboard service"; exit 1; }
     echo "System updated."
 else
     echo "System is up to date at commit hash ${current_hash}."
