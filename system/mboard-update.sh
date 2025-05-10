604360f37803995b9c768ad9f1c216f205fe0635
Eric Smith <eric@esmithy.net>
2025-05-10 17:39:27 -0600

    Increase missionaries on names page.

--- system/mboard-update.sh
+++ system/mboard-update.sh
@@ -12,10 +12,10 @@ current_hash=$(git rev-parse HEAD)
 latest_hash=$(git ls-remote origin -h refs/heads/main | cut -f1)
 
 if [ "$current_hash" != "$latest_hash" ]; then
-    echo "System update available. Updating to commit hash ${latest_hash}..."
+    echo "Application update available. Updating to commit hash ${latest_hash}..."
     git pull || { echo "Failed to pull latest changes"; exit 1; }
     ./install.sh || { echo "Failed to run install script"; exit 1; }
-    echo "System updated."
+    echo "Application updated."
 else
-    echo "System is up to date at commit hash ${current_hash}."
+    echo "Application is up to date at commit hash ${current_hash}."
 fi
